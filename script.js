59a1da7dff547e0a3ff5e9a69f26ab9215b6afa6
unknown <fernandamsino@gmail.com>
2022-06-18 18:05:52 -0300

    css body criado

---
+++ script.js
@@ -0,0 +1,9 @@
+const card_versions = [
+    'imagens/bobrossparrot.gif',
+    'imagens/explodyparrot.gif',
+    'imagens/fiestaparrot.gif', 
+    'imagens/metalparrot.gif',
+    'imagens/revertitparrot', 
+    'imagens/tripletsparrot',
+    'imagens/tripletsparrot',
+    'imagens/unicornparrot.gif'];
