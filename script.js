29af22b42babd4de1b61014963ddb912a3930041
unknown <fernandamsino@gmail.com>
2022-06-19 15:56:31 -0300

    centralixando cartas no css,css da carta criado

--- script.js
+++ script.js
@@ -12,11 +12,18 @@ let number_of_cards=0;
 let numer_of_plays=0;
 let number_of_pairs=0;
 
+const space_for_cards = document.querySelector('memory-game')
+
+
+function comparador(){
+    return Math.random() - 0.5;
+}
+
 function startGame(){
     number_of_cards = prompt('Escolha a quantidade de cartas que deseja jogar: de 4 a 14');
     while (isNaN(number_of_cards) || number_of_cards<4 || number_of_cards>14 || number_of_cards%2 !=0){
         startGame();
     }
-    alert("voce pode jogar agora");
+    
 }
 startGame();
