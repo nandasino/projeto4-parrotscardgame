7c0a6d80c3c8d4791983290521509d5dded709e7
unknown <fernandamsino@gmail.com>
2022-06-19 14:50:16 -0300

    testando funcao de pedir carta

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ const card_versions = [
     'imagens/tripletsparrot',
     'imagens/tripletsparrot',
     'imagens/unicornparrot.gif'];
+
+let number_of_cards=0;
+let numer_of_plays=0;
+let number_of_pairs=0;
+
+function startGame(){
+    number_of_cards = prompt('Escolha a quantidade de cartas que deseja jogar: de 4 a 14');
+    while (isNaN(number_of_cards) || number_of_cards<4 || number_of_cards>14 || number_of_cards%2 !=0){
+        startGame();
+    }
+    alert("voce pode jogar agora");
+}
+startGame();
